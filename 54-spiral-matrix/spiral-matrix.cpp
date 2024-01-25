d6bd439d9d03724a6a05369401969f8145b77f6b
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-01-26 00:24:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (7.34%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int>ans;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int total_element = m*n;
+
+        int startingRow = 0;
+        int endingCol = n - 1;
+        int endingRow = m - 1;
+        int startingCol = 0;
+
+        int count = 0;
+
+        while(count < total_element)
+        {
+            //starting row
+            for(int i = startingCol ; i <=endingCol && count < total_element; i++)
+            {
+                ans.push_back(matrix[startingRow][i]);
+                count++;
+            }
+            startingRow++;
+
+            //ending col
+            for(int i = startingRow ; i <= endingRow && count < total_element; i++)
+            {
+                ans.push_back(matrix[i][endingCol]);
+                count++;
+            }
+            endingCol--;
+
+            //ending row
+            for(int i = endingCol ; i >= startingCol && count < total_element; i--)
+            {
+                ans.push_back(matrix[endingRow][i]);
+                count++;
+            }
+            endingRow--;
+
+            //starting col
+            for(int i = endingRow ; i >= startingRow && count < total_element; i--)
+            {
+                ans.push_back(matrix[i][startingCol]);
+                count++;
+            }
+            startingCol++;
+        }
+        return ans;
+    }
+};
