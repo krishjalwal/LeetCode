dd6bf00c3e055af6066239f8eeb8ab8d6da88ab5
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-01-28 15:57:10 +0530

    Time: 3 ms (52.96%) | Memory: 7.1 MB (18.62%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -7,43 +7,45 @@ public:
             hash[s[i] - 'a']++;
         }
 
-        char most_freq;
-        int max_freq = INT_MIN;
-        for(int i = 0 ; i < 26 ; i++)
-        {
-            if(hash[i] > max_freq)
-            {
-                max_freq = hash[i];
-                most_freq = i + 'a';
-            }
-        }
-
-        int index = 0 ; 
-        while(max_freq > 0 && index < s.size())
-        {
-            s[index] = most_freq;
-            max_freq --;
-            index = index + 2;
-        }
-
-        if(max_freq != 0)
-        {
-            return "";
-        }
-
-        hash[most_freq - 'a'] = 0;
+        //most frequent
+        char most_frequent;
+        int frequent = INT_MIN;
 
         for(int i = 0 ; i < 26 ; i++)
         {
-            while(hash[i] > 0)
+            if(hash[i] > frequent)
             {
-                index = index >= s.size() ? 1 : index;
-                s[index] = i + 'a';
-                hash[i]--;
-                index = index + 2;
-
+                frequent = hash[i];
+                most_frequent = i + 'a';
             }
         }
+
+        int index = 0 ;
+        while(frequent >0 && index < s.size())
+            {
+                s[index] = most_frequent;
+                frequent--;
+                index+=2;
+            }
+
+            if(frequent != 0)
+            {
+                return "";
+            }
+
+            hash[most_frequent - 'a'] = 0;
+
+            for(int i = 0 ; i < 26 ; i++)
+            {
+                while(hash[i]>0)
+                {
+                    index = index >= s.size() ? 1 : index;
+                    s[index] = i + 'a';
+                    hash[i]--;
+                    index+=2;
+                }
+            }
         return s;
+
     }
 };
