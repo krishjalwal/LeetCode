5d3e671029d0d0cf3b75443c276f076f1accd611
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-01-29 11:39:07 +0530

    Time: 7 ms (60.91%) | Memory: 9.9 MB (19.81%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     string intToRoman(int num) {
-     //using pairs:
-        vector<pair<int, string>> map = {
-            {1000, "M"},
-            {900, "CM"},
-            {500, "D"},
-            {400, "CD"},
-            {100, "C"},
-            {90, "XC"},
-            {50, "L"},
-            {40, "XL"},
-            {10, "X"},
-            {9, "IX"},
-            {5, "V"},
-            {4, "IV"},
-            {1, "I"}
-        };
+        string roman[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int values[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string ans = "";
 
-        string answer = "";
-
-        for (auto it : map) {
-            while (num >= it.first) {
-                answer += it.second;
-                num -= it.first;
+        for(int i = 0 ; i < 13; i++)
+        {
+            while(num >= values[i])
+            {
+                ans += roman[i];
+                num = num - values[i];
             }
         }
-        return answer;
+        return ans;
     }
 };
