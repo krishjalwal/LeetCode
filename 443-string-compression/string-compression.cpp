e98b81b06e8118bfc140d8416f4d8eebde9a78b1
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-01-29 11:08:05 +0530

    Time: 4 ms (51.97%) | Memory: 12.7 MB (6.22%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int index = 0; // Index to keep track of the current position in the modified array
-
-        for (int i = 0; i < chars.size(); ) {
-            char current_char = chars[i];
+        int index = 0;
+        for(int i = 0 ; i < chars.size() ;)
+        {
+            char current = chars[i];
             int count = 0;
-
-            // Count consecutive repeating characters
-            while (i < chars.size() && chars[i] == current_char) {
-                ++i;
-                ++count;
+            while(i < chars.size() && chars[i]==current)
+            {
+                i++;
+                count++;
             }
 
-            // Update the array with the compressed characters
-            chars[index++] = current_char;
+            chars[index++] = current;
 
-            if (count > 1) {
+
+            if(count > 1)
+            {
                 string count_str = to_string(count);
-                for (char digit : count_str) {
+                for(char digit : count_str)
+                {
                     chars[index++] = digit;
                 }
             }
         }
-
         return index;
     }
-    
 };
