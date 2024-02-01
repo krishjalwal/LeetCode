639d1ebab86d5d9c658ebe18d171264dcdf9c11a
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-02-01 18:51:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (66.91%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-    int sum1 = 0;  // Maximum amount of money that can be robbed from houses with even indices
-    int sum2 = 0;  // Maximum amount of money that can be robbed from houses with odd indices
+        int sum1 = 0;
+        int sum2 = 0;
 
-    for (int i = 0; i < nums.size(); ++i) {
-        if (i % 2 == 0) {
-            sum1 = max(sum1 + nums[i], sum2);
-        } else {
-            sum2 = max(sum2 + nums[i], sum1);
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i % 2 == 0) {
+                sum1 = max(sum1 + nums[i], sum2);
+            } else {
+                sum2 = max(sum2 + nums[i], sum1);
+            }
         }
-    }
-
-    // Return the maximum money that can be robbed without triggering the alarm
-    return max(sum1, sum2);
+        return max(sum1, sum2);
     }
 };
