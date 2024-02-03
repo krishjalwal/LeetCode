63491a825f64e984abbc05b7524a4cf074c75013
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-02-04 00:14:54 +0530

    Time: 87 ms (43.99%) | Memory: 70.2 MB (20.93%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,40 +1,17 @@
 class Solution {
 public:
-
-    int maxSubArrayHelper(vector<int>&v , int start , int end)
+    int maxSubArray(vector<int>& nums) 
     {
-        if (start == end) {
-            return v[start]; // base case: single element
-        }
+        int maxtillnow=nums[0];
+        int maxsum=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            maxtillnow = max(nums[i], maxtillnow+nums[i]);
 
-        int maxLeftBorderSum = INT_MIN; 
-        int maxRightBorderSum = INT_MIN;
-        int mid = start + ((end - start) >> 1);
-
-        int maxLeftSum = maxSubArrayHelper(v, start, mid);
-        int maxRightSum = maxSubArrayHelper(v, mid + 1, end);
-
-        int leftSum = 0; 
-        int rightSum = 0;
-
-        // Calculate maxLeftBorderSum
-        for (int i = mid; i >= start; i--) {
-            leftSum += v[i];
-            if (leftSum > maxLeftBorderSum) maxLeftBorderSum = leftSum;
-        }
-
-        // Calculate maxRightBorderSum
-        for (int i = mid + 1; i <= end; i++) {
-            rightSum += v[i];
-            if (rightSum > maxRightBorderSum) maxRightBorderSum = rightSum;
-        }
-
-        int crossSum = maxLeftBorderSum + maxRightBorderSum;
-
-        return max(crossSum, max(maxLeftSum, maxRightSum));
-    }
-
-    int maxSubArray(vector<int>& nums) {
-        return maxSubArrayHelper(nums, 0, nums.size() - 1);
+            if(maxtillnow>maxsum)
+                maxsum=maxtillnow;
+        }        
+        return maxsum;
+            
     }
 };
