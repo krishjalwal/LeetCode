a6732221ac5c9254a0961570dd4f80bef6a27dda
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-02-05 23:25:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (8.48%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -2,16 +2,24 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int count = 0 ;
+        unordered_map<char,int> mp;
+
+        for(auto ch : stones)
+        {
+            mp[ch]++;
+        }
+
         for(int i = 0 ; i < jewels.size() ; i++)
         {
-            for(int j = 0 ; j < stones.size() ; j++)
+            for(auto it : mp)
             {
-                if(jewels[i]==stones[j])
+                if(it.first == jewels[i])
                 {
-                    count++;
+                    count += it.second;
                 }
             }
         }
+
         return count;
     }
 };
