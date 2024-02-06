e4156e4c9f63f292815144769c0097308c8ea63d
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-02-06 23:58:25 +0530

    Time: 16 ms (73.26%) | Memory: 33 MB (17.71%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -3,8 +3,8 @@ public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int> ans;
         for (int i = 0; i < words.size(); i++) {
-            for (int j = 0; j < words[i].size(); j++) {
-                if (x == words[i][j]) {
+            for (auto j : words[i]) {
+                if (x == j) {
                     ans.push_back(i);
                     break;
                 }
