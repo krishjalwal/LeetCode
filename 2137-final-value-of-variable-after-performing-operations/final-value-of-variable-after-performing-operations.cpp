15a16c032d279a103231433a06d5a309666d6aca
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-02-07 23:36:14 +0530

    Time: 12 ms (19.04%) | Memory: 17.6 MB (27.96%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
-        int ans = 0 ;
-        for(int i  = 0 ; i < operations.size() ; i++)
-        {
-            for(auto it : operations[i])
-            {
-                if(it=='-')
-                {
+        int ans = 0;
+        for (int i = 0; i < operations.size(); i++) {
+            for (auto it : operations[i]) {
+                if (it == '-') {
                     ans--;
                     break;
-                }
-                else if(it == '+')
-                {
+                } else if (it == '+') {
                     ans++;
                     break;
                 }
-                else
-                {
-                    ans = ans;
-                }
             }
         }
-        return ans ;
+        return ans;
     }
 };
