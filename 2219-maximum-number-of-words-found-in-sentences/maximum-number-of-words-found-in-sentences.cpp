7a417404d6bce658256f5718207750591eb39346
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-02-08 23:44:41 +0530

    Time: 15 ms (13.21%) | Memory: 13.5 MB (5.07%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
-        
+
         int maxcount = 0;
-        for(auto it : sentences)
-        {
+        for (auto it : sentences) {
             int count = 0;
-            for(int i = 0 ; i < it.size() ; i++)
-            {
-                if(it[i]==' ')
-                {
+            for (int i = 0; i < it.size(); i++) {
+                if (it[i] == ' ') {
                     count = count + 1;
                 }
             }
-            if(count > maxcount)
-            {
+            if (count > maxcount) {
                 maxcount = count;
             }
         }
