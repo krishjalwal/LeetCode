2a0904c37c3724cfc32ab30ac4a21909cad63314
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-02-10 00:57:02 +0530

    Time: 3 ms (55.04%) | Memory: 8.8 MB (10.08%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        if(sentence.size()<26)return false;
-        
-        unordered_map<char,int> mp;
+        if (sentence.size() < 26)
+            return false;
+
+        unordered_map<char, int> mp;
 
         vector<char> chr;
 
-        for(char ch = 'a' ; ch <='z' ; ch++)
-        {
+        for (char ch = 'a'; ch <= 'z'; ch++) {
             chr.push_back(ch);
         }
 
-        for(auto it : chr)
-        {
+        for (auto it : chr) {
             mp[it]++;
         }
 
-        for(auto it : sentence)
-        {
+        for (auto it : sentence) {
             mp[it]--;
         }
 
         bool flag = true;
 
-        for(auto it : mp)
-        {
-            if(it.second==1)
-            {
+        for (auto it : mp) {
+            if (it.second == 1) {
                 return false;
             }
         }
