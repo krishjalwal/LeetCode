24e8fc24817a7eb933177e32a2642067cd2519e4
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-02-11 00:27:20 +0530

    Time: 1385 ms (5.00%) | Memory: 570.7 MB (5.20%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    bool isPalin(string s, int i , int j)
+    {
+        string str = s.substr(i,j-i+1);
+        string rev = str; 
+        reverse(str.begin(), str.end());
+        if(str==rev)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    int countSubstrings(string s) {
+        int ans = 0;
+
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            for(int j = i ; j < s.size() ; j++)
+            {
+                if(isPalin(s,i,j))
+                {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+
+    }
+};
