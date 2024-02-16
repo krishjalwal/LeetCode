b49c8daf0e5169574a8dd1e267fea6b8a7a27170
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-02-17 00:21:03 +0530

    Time: 82 ms (98.61%) | Memory: 49.2 MB (97.45%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-        int count = 0;
+        vector<int> vec{1};
 
-        unordered_map<int,int>mp;
+        sort(arr.begin(),arr.end());
 
-        for(auto it : arr)
+        for(int i = 1 ; i < arr.size() ; i++)
         {
-            mp[it]++;
-        }
-
-        vector<pair<int,int>> vec(mp.begin(),mp.end());
-
-        sort(vec.begin(),vec.end(),[](const auto&a,const auto&b)
-        {
-            return a.second < b.second;
-        });
-
-        
-
-        for(auto& pair : vec) {
-            if(k >= pair.second) {
-                k -= pair.second;
-                pair.second = 0;
-            } else {
-                pair.second -= k;
-                k = 0;
-            }
-        }
-
-        for(auto &pair : vec)
-        {
-            if(pair.second > 0)
+            if(arr[i]==arr[i-1])
             {
-                count++;
+                vec.back()++;
+            }
+            else
+            {
+                vec.push_back(1);
             }
         }
 
-        return count;
+        sort(vec.begin(),vec.end());
 
+        int ans = vec.size();
+
+        for(auto it : vec)
+        {
+            if(k>=it)
+            {
+                ans--;
+                k = k - it;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return ans;
     }
 };
