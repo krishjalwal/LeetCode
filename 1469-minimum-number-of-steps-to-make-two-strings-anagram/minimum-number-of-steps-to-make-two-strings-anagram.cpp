e84035070e9e9e4eaf86e26c8e86779e7d774ec0
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-02-19 01:41:10 +0530

    Time: 68 ms (36.97%) | Memory: 19 MB (5.37%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -3,26 +3,21 @@ public:
     int minSteps(string s, string t) {
         unordered_map<char, int> freq;
 
-        // Count frequency of characters in string s
         for (char c : s) {
             freq[c]++;
         }
-        for(char c:t){
+        for (char c : t) {
             freq[c]--;
         }
 
         int minSteps = 0;
 
-        // Sum up the absolute differences between frequencies
         for (const auto& pair : freq) {
-            if(pair.second > 0)
-            {
+            if (pair.second > 0) {
                 minSteps += pair.second;
             }
-             // Add the absolute difference in frequencies
         }
 
-        // Since the strings are of the same length, we need to divide by 2 to get the number of steps
         return minSteps;
     }
 };
