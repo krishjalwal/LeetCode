86ec630d183cac659dbcf21d12b46a34ebf568c5
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-02-20 00:01:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (19.05%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    bool power(int n, long long int i)
-    {
-        if(i == n)
-        {
-            return true;
-        }
-        else if(i > n)
-        {
-            return false;
-        }
-        else
-        {
-            return power(n, i * 2LL); // Use long long int to avoid overflow
-        }
-    }
     bool isPowerOfTwo(int n) {
-        return power(n, 1);
+        int ans = 1;
+        for (int i = 0; i < 30; i++) {
+            if (ans == n) {
+                return true;
+            }
+            ans = ans * 2;
+            if (ans == n) {
+                return true;
+            }
+        }
+        return false;
     }
 };
