ca4abe9a86d6b2729784c367056d92901ac6c509
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-02-21 00:18:22 +0530

    Time: 6 ms (26.50%) | Memory: 8.6 MB (21.44%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
         if ((str1 + str2) != (str2 + str1)) {
-            return ""; // If concatenation doesn't match, return empty string
+            return ""; 
         }
 
         // Find the GCD
         while (str1 != str2) {
             if (str1.length() > str2.length()) {
-                str1 = str1.substr(str2.length()); // Remove the common prefix
+                str1 = str1.substr(str2.length());
             } else {
-                str2 = str2.substr(str1.length()); // Remove the common prefix
+                str2 = str2.substr(str1.length());
             }
         }
 
-        return str1; // or str2, since they are equal
+        return str1;
     }
 };
