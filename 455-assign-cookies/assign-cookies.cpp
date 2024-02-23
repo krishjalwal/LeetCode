2b95d36dbb94ceeb86e3fe58f689445b3d4dc7b5
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-02-24 01:16:25 +0530

    Time: 23 ms (82.96%) | Memory: 20.7 MB (5.38%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int count = 0; int k = 0;
-        sort(g.begin(),g.end());
-        sort(s.begin(),s.end());
-
-        for (auto greed : g) {
-            for (int j = k ; j < s.size() ; j++) {
-                if (s[j] >= greed)
-                {
-                    count++;
-                    s[j]=0;
-                    break;
-                }
+        sort(begin(g), end(g));
+        sort(begin(s), end(s));
+        int i=0,j=0;
+        while(i<g.size() && j<s.size())
+        {
+            if(g[i]<=s[j])
+            {
+                i++;
             }
+            j++;
         }
-
-        if (count > 0) {
-            return count;
-        }
-        return 0;
+        return i;
     }
 };
