5e2984208b8cceb649594916ae2c54ced54845e2
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-02-29 00:50:50 +0530

    Time: 129 ms (62.71%) | Memory: 112.6 MB (11.89%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double sum = 0;
+        for (int i = 0; i < k; ++i) {
+            sum += nums[i];
+        }
+
+        double maxAvg = sum / k;
+
+        for (int j = k; j < nums.size(); ++j) {
+            sum += nums[j] - nums[j - k];
+            maxAvg = max(maxAvg, sum / k);
+        }
+
+        return maxAvg;
+    }
+};
