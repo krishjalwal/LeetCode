54eae6d3005f7bd7437a34dfa95acfe5747ca3cd
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-03-14 02:38:26 +0530

    Time: 43 ms (8.07%) | Memory: 7.3 MB (15.62%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+
+        int sum1 = 0 ; int sum2 = 0;
+
+        for(int i = 1 ; i <= n ; i++)
+        {
+            sum1 = 0;
+            sum2 = 0;
+
+            for(int j = 1 ; j <= i ; j++)
+            {
+                sum1 += j;
+            }
+            for(int k = i ; k <= n ; k++)
+            {
+                sum2 += k;
+            }
+            if(sum1==sum2)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
