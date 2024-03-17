6852edd0d40b186fab7389d7568f72e2ebdaa799
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-03-17 23:47:17 +0530

    Time: 3 ms (38.71%) | Memory: 7.9 MB (37.81%) - LeetSync

--- 290-word-pattern/word-pattern.cpp
+++ 290-word-pattern/word-pattern.cpp
@@ -1,44 +1,36 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        map<string,char>mp1;
-        map<char,string>mp2;
+        map<string, char> mp1;
+        map<char, string> mp2;
 
         vector<string> v;
 
         string temp = "";
         int n = s.size();
-        for(int i = 0 ; i < n ; i++)
-        {
-            if(s[i] == ' ')
-            {
+        for (int i = 0; i < n; i++) {
+            if (s[i] == ' ') {
                 v.push_back(temp);
-                temp="";
-            }
-            else
-            {
+                temp = "";
+            } else {
                 temp += s[i];
             }
-        }v.push_back(temp);
+        }
+        v.push_back(temp);
 
-        if(v.size() != pattern.size())
-        {
+        if (v.size() != pattern.size()) {
             return false;
         }
 
         string t = "";
         int vsize = v.size();
-        for(int i = 0 ; i < vsize ; i++)
-        {
-            if(mp1.find(v[i]) == mp1.end() and mp2.find(pattern[i])==mp2.end())
-            {
+        for (int i = 0; i < vsize; i++) {
+            if (mp1.find(v[i]) == mp1.end() and
+                mp2.find(pattern[i]) == mp2.end()) {
                 mp1[v[i]] = pattern[i];
                 mp2[pattern[i]] = v[i];
-            }
-            else
-            {
-                if(mp1[v[i]] != pattern[i] or mp2[pattern[i]] != v[i])
-                {
+            } else {
+                if (mp1[v[i]] != pattern[i] or mp2[pattern[i]] != v[i]) {
                     return false;
                 }
             }
