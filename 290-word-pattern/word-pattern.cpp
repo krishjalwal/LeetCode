5d775b8ff505992c595d949c7146394f59e00e35
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-03-17 23:47:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (37.81%) - LeetSync

--- 290-word-pattern/word-pattern.cpp
+++ 290-word-pattern/word-pattern.cpp
@@ -1,40 +1,27 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        map<string, char> mp1;
-        map<char, string> mp2;
-
-        vector<string> v;
-
-        string temp = "";
-        int n = s.size();
-        for (int i = 0; i < n; i++) {
-            if (s[i] == ' ') {
-                v.push_back(temp);
-                temp = "";
-            } else {
-                temp += s[i];
+        map<char, string> mp;
+        set<string> st;
+        int n1 = pattern.length();
+        int n2 = s.length();
+        int j = 0;
+        for (int i = 0; i < n1; i++) {
+            string tt = "";
+            for (j; j < n2 && s[j] != ' '; j++) {
+                tt += s[j];
+            }
+            j++;
+            if (mp.find(pattern[i]) == mp.end() && st.count(tt) == 0) {
+                mp[pattern[i]] = tt;
+                st.insert(tt);
             }
         }
-        v.push_back(temp);
-
-        if (v.size() != pattern.size()) {
-            return false;
+        string ts = "";
+        for (int i = 0; i < n1; i++) {
+            ts += mp[pattern[i]];
+            if (i != n1 - 1) ts += ' ';
         }
-
-        string t = "";
-        int vsize = v.size();
-        for (int i = 0; i < vsize; i++) {
-            if (mp1.find(v[i]) == mp1.end() and
-                mp2.find(pattern[i]) == mp2.end()) {
-                mp1[v[i]] = pattern[i];
-                mp2[pattern[i]] = v[i];
-            } else {
-                if (mp1[v[i]] != pattern[i] or mp2[pattern[i]] != v[i]) {
-                    return false;
-                }
-            }
-        }
-        return true;
+        return ts == s;
     }
 };
