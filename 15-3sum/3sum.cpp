23378fbf2dcfb78b2c1395541662c7c96e317ec5
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-03-28 23:12:23 +0530

    Time: 1185 ms (17.95%) | Memory: 293.7 MB (20.38%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,47 +1,28 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end()); // Sort the input array
-
-        int n = nums.size();
-
-        for(int i = 0 ; i < n - 2; i++) {
-            // Skip duplicates for i
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    ans.push_back({nums[i], nums[left], nums[right]});
-                    // Skip duplicates for left and right pointers
-                    while (left < right && nums[left] == nums[left + 1])
-                        left++;
-                    while (left < right && nums[right] == nums[right - 1])
-                        right--;
-
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
+        int target = 0;
+        sort(nums.begin(), nums.end());
+        set<vector<int>> s;
+        vector<vector<int>> output;
+        for (int i = 0; i < nums.size(); i++){
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    s.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
                 } else {
-                    right--;
+                    k--;
                 }
             }
-
-            // Additional optimization: If nums[i] is already positive, no need to continue
-            if (nums[i] > 0)
-                break;
         }
-
-        return ans;
+        for(auto triplets : s)
+            output.push_back(triplets);
+        return output;
     }
 };
