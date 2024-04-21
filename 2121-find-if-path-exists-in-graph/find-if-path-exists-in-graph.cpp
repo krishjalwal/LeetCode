d53a4c6b318bd340f46c4add2b4685c81fa73bb5
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-04-21 12:31:58 +0530

    Time: 468 ms (56.24%) | Memory: 189.4 MB (46.18%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    bool dfs(vector<vector<int>>&graph , vector<int>&visited , int source , int destination)
+    {
+        //Base case
+        if(source == destination)
+        {
+            return true;
+        }
+        if(visited[source])
+        {
+            return false;
+        }
+
+        //make visited.
+        visited[source] = 1;
+
+
+        //traversal
+        for(int i = 0 ;  i < graph[source].size()  ;i++)
+        {
+            if(dfs(graph,visited,graph[source][i],destination))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        
+        vector<vector<int>>graph(n);
+
+        for(int i = 0 ; i < edges.size(); i++)
+        {
+            graph[edges[i][0]].push_back(edges[i][1]);
+            graph[edges[i][1]].push_back(edges[i][0]);
+        }
+
+        vector<int> visited(n,0);
+        return dfs(graph,visited,source,destination);
+
+    }
+};
