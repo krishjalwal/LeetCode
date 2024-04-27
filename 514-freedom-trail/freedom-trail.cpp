93e235ed1aea367b4d8510b87908b47f4ff61762
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2024-04-27 11:17:45 +0530

    Time: 207 ms (10.14%) | Memory: 14.4 MB (63.21%) - LeetSync

--- 514-freedom-trail/freedom-trail.cpp
+++ 514-freedom-trail/freedom-trail.cpp
@@ -1,34 +1,51 @@
 class Solution {
 public:
-    int dp[101][101];
-    int helper(string& ring,string& key,int idx1,int idx2,map<char,vector<int>>& mp){
-        int n = ring.size();
-        if(idx2==key.size()){
-            return 0;
-        }
-        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
-        if(ring[idx1]==key[idx2]){
-            return 1+helper(ring,key,idx1,idx2+1,mp);
-        }
-        int ans = INT_MAX;
-        char tofind = key[idx2];
-        vector<int> allIdx = mp[tofind];
-        
-        for(auto it : allIdx){
-            int mindist = min(abs(it-idx1),n-abs(it-idx1));
-            int tempans = helper(ring,key,it,idx2,mp);
-            if(tempans!=INT_MAX)
-            ans = min(ans,mindist+tempans);
-        }
-        return dp[idx1][idx2] = ans;
-    }
-
     int findRotateSteps(string ring, string key) {
-        memset(dp,-1,sizeof(dp));
-        map<char,vector<int>> mp;
-        for(int i = 0;i<ring.size();i++){
-            mp[ring[i]].push_back(i);
+        int n = ring.size();
+        int m = key.size();
+        vector<vector<int>>dp(n+5,vector<int>(m+5,1e6));
+        dp[1][0] = 0;
+        int i,j,k;
+        for(j = 0;j<m;j++)
+        {
+            for(i = 1;i<=n;i++)
+            {
+                //we need to press key[j] now.
+                for(k = i;k<=n;k++)
+                {
+                    if(ring[k-1] != key[j])continue;
+                    int dist;
+                    if(k >= i)
+                    {
+                        dist = min(k - i, n - k + i);
+                    }
+                    else
+                    {
+                        dist = min(i - k, n - i + k);
+                    }
+                    dp[k][j+1] = min(dp[k][j+1], dist + 1 + dp[i][j]);
+                }
+                for(k = i-1;k>0;k--)
+                {
+                    if(ring[k-1] != key[j])continue;
+                    int dist;
+                    if(k >= i)
+                    {
+                        dist = min(k - i, n - k + i);
+                    }
+                    else
+                    {
+                        dist = min(i - k, n - i + k);
+                    }
+                    dp[k][j+1] = min(dp[k][j+1], dist + 1 + dp[i][j]);
+                }
+            }
         }
-        return helper(ring,key,0,0,mp);
+        int ans = 1e6;
+        for(i = 1;i<=n;i++)
+        {
+            ans = min(ans, dp[i][m]);
+        }
+        return ans;
     }
 };
