091ef743c30730a09c0d48da34136303eadea665
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:06:14 +0530

    Create 1371-MinimumRemoveToMakeValidParentheses

---
+++ 1371-MinimumRemoveToMakeValidParentheses/1371-MinimumRemoveToMakeValidParentheses.cpp
@@ -0,0 +1,62 @@
+// Last updated: 8/9/2026, 12:06:14 AM
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        int count = 0 ;
+
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            if(s[i]=='(')
+            {
+                count++;
+            }
+            else if(s[i]==')')
+            {
+                if(count > 0)
+                {
+                    count--;
+                }
+                else
+                {
+                    s[i]=0;
+                }
+            }
+        }
+
+        count = 0;
+
+        for(int i = s.size()-1 ; i >= 0 ; i--)
+        {
+            if(s[i] == ')')
+            {
+                count++;
+            }
+            else if(s[i] == '(')
+            {
+                if(count > 0)
+                {
+                    count--;
+                }
+                else
+                {
+                    s[i] = 0;
+                }
+            }
+        }
+
+        string ans = "";
+
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            if(s[i]!=0)
+            {
+                ans += s[i];
+            }
+        }
+
+        return ans;
+
+
+        
+    }
+};
