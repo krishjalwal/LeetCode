f2ccfabf000a9e079c7f391a630aafee89aa338d
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:12:38 +0530

    Create 98-ValidateBinarySearchTree

---
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/9/2026, 12:12:37 AM
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValidBSTHelper(root, LONG_MIN, LONG_MAX);
+    }
+    
+    bool isValidBSTHelper(TreeNode* node, long minVal, long maxVal) {
+        if (node == nullptr)
+            return true;
+        
+        if (node->val <= minVal || node->val >= maxVal)
+            return false;
+        
+        return isValidBSTHelper(node->left, minVal, node->val) &&
+               isValidBSTHelper(node->right, node->val, maxVal);
+    }
+};
