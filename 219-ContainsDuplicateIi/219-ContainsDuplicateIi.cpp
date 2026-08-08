c4479ef9e3a13cec59029d379e87f266a5251f5f
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:11:23 +0530

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/9/2026, 12:11:23 AM
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+        unordered_map<int, int> hashing;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            // Check if the element is already in the hash map and if the difference between indices is less than or equal to k
+            if (hashing.count(nums[i]) > 0 && i - hashing[nums[i]] <= k) {
+                return true;
+            }
+            hashing[nums[i]] = i;
+        }
+
+        return false;
+    }
+};
