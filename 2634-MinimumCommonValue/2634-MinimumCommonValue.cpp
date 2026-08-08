f400f583f7716be6373414fc81e8f0a2cc20e1ff
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:03:13 +0530

    Create 2634-MinimumCommonValue

---
+++ 2634-MinimumCommonValue/2634-MinimumCommonValue.cpp
@@ -0,0 +1,36 @@
+// Last updated: 8/9/2026, 12:03:13 AM
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        // for(int i = 0 ; i < nums1.size() ; i++)
+        // {
+        //     for(int j = 0 ; j < nums2.size() ; j++)
+        //     {
+        //         if(nums1[i]==nums2[j])
+        //         {
+        //             return nums1[i];
+        //         }
+        //     }
+        // }
+        // return -1;
+
+        int i = 0 ; int j = 0 ;
+
+        while(i < nums1.size() && j < nums2.size())
+        {
+            if(nums1[i]==nums2[j])
+            {
+                return nums1[i];
+            }
+            else if(nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        return -1;
+    }
+};
