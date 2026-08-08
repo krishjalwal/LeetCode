84c13c9ea5e7cf2b9116744075d7771dbc464eab
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:05:17 +0530

    Create 1741-SortArrayByIncreasingFrequency

---
+++ 1741-SortArrayByIncreasingFrequency/1741-SortArrayByIncreasingFrequency.cpp
@@ -0,0 +1,29 @@
+// Last updated: 8/9/2026, 12:05:17 AM
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        int n=nums.size();
+        map<int,int> mp;
+        map<int,vector<int>> pkp;
+        for(int i=0;i<n;i++){
+           mp[nums[i]]++;
+        }
+        for(int i=0;i<n;i++){
+          pkp[mp[nums[i]]].push_back(nums[i]);
+        //   int kk=mp[s[i]];
+        //   cout<<kk<<endl;
+        }
+
+        vector<int> ans;
+
+        for(auto it:pkp){
+            sort(it.second.begin(),it.second.end());
+            
+            for(int i=it.second.size()-1;i>=0;i--){
+                ans.push_back(it.second[i]);
+            }
+        }
+
+        return ans;
+    }
+};
