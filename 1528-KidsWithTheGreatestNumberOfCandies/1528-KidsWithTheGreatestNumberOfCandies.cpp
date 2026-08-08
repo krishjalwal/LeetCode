0b99b9af2943236726bdf3cbe6d69af45c2696e3
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:05:56 +0530

    Create 1528-KidsWithTheGreatestNumberOfCandies

---
+++ 1528-KidsWithTheGreatestNumberOfCandies/1528-KidsWithTheGreatestNumberOfCandies.cpp
@@ -0,0 +1,15 @@
+// Last updated: 8/9/2026, 12:05:56 AM
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> v(candies.size(),false);
+        int maxx=INT_MIN;
+        for(int i=0;i<candies.size();i++){
+            maxx=max(candies[i],maxx);
+        }
+        for(int i=0;i<candies.size();i++){
+            v[i]=(candies[i]+extraCandies>=maxx);
+        }
+        return v;
+    }
+};
