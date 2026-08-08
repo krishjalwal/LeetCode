9d19b4fb6425c42a7891a80ca312e5e6add03f4f
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:08:18 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.cpp
@@ -0,0 +1,26 @@
+// Last updated: 8/9/2026, 12:08:18 AM
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count = 0 ;
+        unordered_map<char,int> mp;
+
+        for(auto ch : stones)
+        {
+            mp[ch]++;
+        }
+
+        for(int i = 0 ; i < jewels.size() ; i++)
+        {
+            for(auto it : mp)
+            {
+                if(it.first == jewels[i])
+                {
+                    count += it.second;
+                }
+            }
+        }
+
+        return count;
+    }
+};
