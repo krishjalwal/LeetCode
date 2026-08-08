12a20500eb9b780647e39fd6f5182ed293c9c2bb
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:10:27 +0530

    Create 347-TopKFrequentElements

---
+++ 347-TopKFrequentElements/347-TopKFrequentElements.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/9/2026, 12:10:27 AM
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> arr;
+        unordered_map<int, int> hashing;
+
+        for(auto it : nums) {
+            hashing[it]++;
+        }
+
+        
+        vector<pair<int, int>> frequencyPairs;
+        for (auto it : hashing) {
+            frequencyPairs.push_back({it.second, it.first});
+        }
+
+       
+        sort(frequencyPairs.rbegin(), frequencyPairs.rend());
+
+     
+        for (int i = 0; i < k; ++i) {
+            arr.push_back(frequencyPairs[i].second);
+        }
+
+        return arr;
+    }
+};
