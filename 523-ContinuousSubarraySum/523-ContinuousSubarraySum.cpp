80360d3bbc05766f01972dd773ce4db42546f44c
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:09:18 +0530

    Create 523-ContinuousSubarraySum

---
+++ 523-ContinuousSubarraySum/523-ContinuousSubarraySum.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/9/2026, 12:09:18 AM
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        map[0] = -1; // To handle the case where the sub-array starts from index 0
+        int sum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            int rem = sum % k;
+
+            if (map.find(rem) != map.end()) {
+                if (i - map[rem] > 1) {
+                    return true;
+                }
+            } else {
+                map[rem] = i;
+            }
+        }
+
+        return false;
+    }
+};
