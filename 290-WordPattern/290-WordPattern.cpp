90acd632ad1f6ad2ed41f1cd258d060d2a682488
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:10:42 +0530

    Create 290-WordPattern

---
+++ 290-WordPattern/290-WordPattern.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/9/2026, 12:10:42 AM
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        map<char, string> mp;
+        set<string> st;
+        int n1 = pattern.length();
+        int n2 = s.length();
+        int j = 0;
+        for (int i = 0; i < n1; i++) {
+            string tt = "";
+            for (j; j < n2 && s[j] != ' '; j++) {
+                tt += s[j];
+            }
+            j++;
+            if (mp.find(pattern[i]) == mp.end() && st.count(tt) == 0) {
+                mp[pattern[i]] = tt;
+                st.insert(tt);
+            }
+        }
+        string ts = "";
+        for (int i = 0; i < n1; i++) {
+            ts += mp[pattern[i]];
+            if (i != n1 - 1) ts += ' ';
+        }
+        return ts == s;
+    }
+};
