40c6f0d439eb2b41618f81c7b6c346dfdbc341ad
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:07:09 +0530

    Create 1070-ConvertToBase2

---
+++ 1070-ConvertToBase2/1070-ConvertToBase2.cpp
@@ -0,0 +1,22 @@
+// Last updated: 8/9/2026, 12:07:08 AM
+class Solution {
+public:
+    string baseNeg2(int n) {
+        if(n==0)
+        {
+            return "0";
+        }
+        
+        string result = "";
+
+        while(n!=0)
+        {
+            int bit = n & 1;
+            result = to_string(bit) + result;
+            n = (n - bit)/(-2);
+        }
+
+        return result;
+       
+    }
+};
