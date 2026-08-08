e0ea9a2cb6318a6065e6986d885bc8a8f3d88f56
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:06:57 +0530

    Create 1146-GreatestCommonDivisorOfStrings

---
+++ 1146-GreatestCommonDivisorOfStrings/1146-GreatestCommonDivisorOfStrings.cpp
@@ -0,0 +1,20 @@
+// Last updated: 8/9/2026, 12:06:57 AM
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if ((str1 + str2) != (str2 + str1)) {
+            return "";
+        }
+
+        // Find the GCD
+        while (str1 != str2) {
+            if (str1.length() > str2.length()) {
+                str1 = str1.substr(str2.length());
+            } else {
+                str2 = str2.substr(str1.length());
+            }
+        }
+
+        return str1;
+    }
+};
