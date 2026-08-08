7e56433c11baddb8faf99137e2e563d625b15c6c
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:05:12 +0530

    Create 1781-CheckIfTwoStringArraysAreEquivalent

---
+++ 1781-CheckIfTwoStringArraysAreEquivalent/1781-CheckIfTwoStringArraysAreEquivalent.cpp
@@ -0,0 +1,14 @@
+// Last updated: 8/9/2026, 12:05:12 AM
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string sum1="0",sum2="0";
+        for(int i=0;i<word1.size();i++){
+            sum1+=word1[i];
+        }
+        for(int i=0;i<word2.size();i++){
+            sum2+=word2[i];
+        }return sum1==sum2;
+    } 
+    
+};
