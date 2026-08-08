8f34cc8089a428ceb6261767b12627e335cfd935
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:10:39 +0530

    Create 330-PatchingArray

---
+++ 330-PatchingArray/330-PatchingArray.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/9/2026, 12:10:38 AM
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long count = 0;
+        long long sum = 0;
+        long long i = 0;
+        while(sum < n){
+            if(i<nums.size() && nums[i] <= (sum+1)){
+                sum += nums[i++];
+            }else{
+                count++;
+                sum += (sum + 1);
+            }
+        }
+        return count;
+    }
+};
