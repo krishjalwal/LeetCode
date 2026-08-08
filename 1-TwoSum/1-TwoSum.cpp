47fab594b26d16e3a485f7cebca4f16f28990059
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:14:51 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.cpp
@@ -0,0 +1,24 @@
+// Last updated: 8/9/2026, 12:14:51 AM
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int toFind = 0;
+        unordered_map<int,int> indexMap;
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            indexMap[nums[i]] = i;
+        }
+
+        for (int i = 0; i < nums.size(); i++) 
+        {
+        toFind = target - nums[i];
+        auto found = indexMap.find(toFind);
+
+        if (found != indexMap.end() && found->second != i) 
+        {
+            return {i, found->second};
+        }
+        }
+        return {};
+    }
+};
