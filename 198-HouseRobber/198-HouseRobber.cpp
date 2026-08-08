091eb27e5bcff5fcd91f0f634332d96273671944
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:11:42 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -0,0 +1,17 @@
+// Last updated: 8/9/2026, 12:11:42 AM
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i % 2 == 0) {
+                sum1 = max(sum1 + nums[i], sum2);
+            } else {
+                sum2 = max(sum2 + nums[i], sum1);
+            }
+        }
+        return max(sum1, sum2);
+    }
+};
