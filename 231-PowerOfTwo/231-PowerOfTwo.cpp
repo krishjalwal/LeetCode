1d0054d82513158078ef2342acf510cc98d0a1bf
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:11:15 +0530

    Create 231-PowerOfTwo

---
+++ 231-PowerOfTwo/231-PowerOfTwo.cpp
@@ -0,0 +1,17 @@
+// Last updated: 8/9/2026, 12:11:15 AM
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int ans = 1;
+        for (int i = 0; i < 30; i++) {
+            if (ans == n) {
+                return true;
+            }
+            ans = ans * 2;
+            if (ans == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
