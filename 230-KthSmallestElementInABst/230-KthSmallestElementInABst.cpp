291a7f0517778673369a5f2c924b5cc23ac7a2aa
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:11:16 +0530

    Create 230-KthSmallestElementInABst

---
+++ 230-KthSmallestElementInABst/230-KthSmallestElementInABst.cpp
@@ -0,0 +1,26 @@
+// Last updated: 8/9/2026, 12:11:16 AM
+class Solution {
+public:
+
+    void traversal(TreeNode* root, vector<int> &arr)
+    {
+        if(root == NULL)
+        {
+            return;
+        }
+
+        traversal(root->left, arr);
+        
+        arr.push_back(root->val); // Push the value of the current node
+        
+        traversal(root->right, arr);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> arr ;
+
+        traversal(root, arr);
+
+        return arr[k-1];
+    }
+};
