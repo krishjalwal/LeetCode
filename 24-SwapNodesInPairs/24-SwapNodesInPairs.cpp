f0ec80bc7209e98b13bdcff0f83596cdabf072a0
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:14:05 +0530

    Create 24-SwapNodesInPairs

---
+++ 24-SwapNodesInPairs/24-SwapNodesInPairs.cpp
@@ -0,0 +1,94 @@
+// Last updated: 8/9/2026, 12:14:04 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+#define null NULL
+#define Node ListNode
+
+    void reverse(Node *head, int size)
+    {
+        int times = size;
+        Node *curr = head;
+        Node *prev = null;
+
+        while(times--)
+        {
+            Node *nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return;
+    }
+
+    ListNode* swapPairs(ListNode* head) {
+        
+        if(head==null or head->next == null)
+        {
+            return head;
+        }
+
+        int size = 2;
+
+        Node *left = head;
+        Node *right = null;
+        Node *prevleft = nullptr;
+        Node *nextleft =nullptr;
+        Node *res = nullptr;
+
+        while(true)
+        {
+            if(left==null)
+            {
+                break;
+            }
+            right = left;
+            
+            for(int i = 0 ; i < size-1 ; i++)
+            {
+                right = right->next;
+            }
+
+            if(right)
+            {
+                if(res==null)
+                {
+                    res = right;
+                }
+                nextleft = right->next;
+                
+                reverse(left,size);
+                if(prevleft)
+                {
+                    prevleft->next = right;
+                }
+                prevleft = left;
+                if(left==null)
+                {
+                    break;
+                }
+                left = nextleft;
+            }
+            else
+            {
+                if(prevleft)
+                {
+                    prevleft->next = left;
+                }
+                
+                break;
+            }
+        }
+        return res;
+    }
+};
