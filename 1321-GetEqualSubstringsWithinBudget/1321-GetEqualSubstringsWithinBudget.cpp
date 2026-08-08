f4deb7941b08fba6c9843728b189949e5a70bb17
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:06:23 +0530

    Create 1321-GetEqualSubstringsWithinBudget

---
+++ 1321-GetEqualSubstringsWithinBudget/1321-GetEqualSubstringsWithinBudget.cpp
@@ -0,0 +1,31 @@
+// Last updated: 8/9/2026, 12:06:23 AM
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        int n=s.size();
+      
+          //abcd and acde
+        // 0,1,1,1   
+       int i=0, j=0, ans=0,sum=0;
+       while(j<n){
+
+        sum+=abs(int(s[j])-int (t[j]));
+
+        if(sum<=maxCost){
+            ans=max(j-i+1,ans);
+            
+        }
+
+        else {
+           while(sum>maxCost && i<=j){
+             sum= sum-abs(int(s[i])-int (t[i]));
+            i++;
+           }
+        }
+        j++;
+
+       }
+
+       return ans;       
+    }
+};
