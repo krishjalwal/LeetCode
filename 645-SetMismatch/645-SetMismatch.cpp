39b92137efc305883e5167fe523811a549db04fc
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:08:48 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.cpp
@@ -0,0 +1,37 @@
+// Last updated: 8/9/2026, 12:08:48 AM
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> answer;
+        vector<int> dubli;
+        for (int i = 1; i <= nums.size(); i++) {
+            dubli.push_back(i);
+        }
+
+        unordered_map<int, int> hashing;
+
+        for (auto it : nums) {
+            hashing[it]++;
+        }
+
+        for (auto it : hashing) {
+            int repeat = it.second;
+            if (repeat > 1) {
+                answer.push_back(it.first); // Push the duplicated number
+            }
+        }
+
+        int result = 0;
+
+        for (int x : dubli) {
+            result ^= x;
+        }
+
+        for (int x : nums) {
+            result ^= x;
+        }
+
+        answer.push_back(result ^ answer[0]); // XOR the duplicated number with the result to find the missing number
+        return answer;
+    }
+};
