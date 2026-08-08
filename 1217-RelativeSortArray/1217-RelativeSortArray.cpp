a0565208f7146dfc765f467ac5d5675b83f970e8
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:06:47 +0530

    Create 1217-RelativeSortArray

---
+++ 1217-RelativeSortArray/1217-RelativeSortArray.cpp
@@ -0,0 +1,24 @@
+// Last updated: 8/9/2026, 12:06:46 AM
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int> result;
+     
+        for (int i = 0; i < arr2.size(); i++) {
+            for (int j = 0; j < arr1.size(); j++) {   
+                if (arr1[j] == arr2[i]) {
+                    result.push_back(arr1[j]);
+                    arr1[j] = -1;
+                }
+            }
+        }
+        sort(arr1.begin(), arr1.end());
+
+        for (int i = 0; i < arr1.size(); i++) {
+            if (arr1[i] != -1) {
+                result.push_back(arr1[i]);
+            }
+        }
+        return result;
+    }
+};
