398eecc64e3accaf865cdff2befc44d45f9ce322
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:13:35 +0530

    Create 48-RotateImage

---
+++ 48-RotateImage/48-RotateImage.cpp
@@ -0,0 +1,16 @@
+// Last updated: 8/9/2026, 12:13:35 AM
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < matrix[0].size(); i++) {
+            vector<int> temp;
+            for (int j = matrix.size() - 1; j >= 0; j--) {
+                temp.push_back(matrix[j][i]);
+            }
+            ans.push_back(temp);
+        }
+        matrix = ans;
+    }
+};
