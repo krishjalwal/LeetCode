ba3474835fdce505e9f6f7f8f63a636c4094311a
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:07:49 +0530

    Create 917-BoatsToSavePeople

---
+++ 917-BoatsToSavePeople/917-BoatsToSavePeople.cpp
@@ -0,0 +1,26 @@
+// Last updated: 8/9/2026, 12:07:49 AM
+class Solution {
+public:
+    int numRescueBoats(std::vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+
+        int count = 0;
+        int left = 0;
+        int right = people.size() - 1;
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                // If both people can fit into a boat, move both pointers inward
+                left++;
+                right--;
+            } else {
+                // If only the person at the 'right' pointer can fit, move only 'right' inward
+                right--;
+            }
+            // In either case, a boat is needed
+            count++;
+        }
+
+        return count;
+    }
+};
