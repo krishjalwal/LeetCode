577c25c1b03aebec6063ecd04d25077da51ea637
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:08:39 +0530

    Create 678-ValidParenthesisString

---
+++ 678-ValidParenthesisString/678-ValidParenthesisString.cpp
@@ -0,0 +1,39 @@
+// Last updated: 8/9/2026, 12:08:39 AM
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> open;
+        stack<int> star;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                open.push(i);
+            }
+
+            else if (s[i] == ')') {
+                if (!open.empty()) {
+                    open.pop();
+                } else if (!star.empty()) {
+                    star.pop();
+                } else {
+                    return false;
+                }
+            }
+
+            else if (s[i] == '*') {
+                star.push(i);
+            }
+        }
+        while (!open.empty()) {
+            if (star.empty()) {
+                return false;
+            } else if (open.top() < star.top()) {
+                open.pop();
+                star.pop();
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
