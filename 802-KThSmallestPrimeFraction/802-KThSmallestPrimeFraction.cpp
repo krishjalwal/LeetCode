68117875ca9a15f05af93243f76daa8401f04f11
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:08:14 +0530

    Create 802-KThSmallestPrimeFraction

---
+++ 802-KThSmallestPrimeFraction/802-KThSmallestPrimeFraction.cpp
@@ -0,0 +1,21 @@
+// Last updated: 8/9/2026, 12:08:14 AM
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        vector<float> vp;
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = i + 1; j < arr.size(); j++) {
+                vp.push_back(((float)arr[i] / (float)arr[j]));
+            }
+        }
+        sort(vp.begin(), vp.end());
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = i + 1; j < arr.size(); j++) {
+                if (vp[k - 1] == (((float)arr[i] / (float)arr[j]))) {
+                    return {arr[i], arr[j]};
+                }
+            }
+        }
+        return {0, 1};
+    }
+};
