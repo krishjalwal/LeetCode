d0585781bb1a9f2fb1dab20d8e2eb2f9fec1b3a5
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:07:12 +0530

    Create 1056-CapacityToShipPackagesWithinDDays

---
+++ 1056-CapacityToShipPackagesWithinDDays/1056-CapacityToShipPackagesWithinDDays.cpp
@@ -0,0 +1,38 @@
+// Last updated: 8/9/2026, 12:07:11 AM
+class Solution {
+public:
+    bool possible(vector<int> w, int d, int m) {
+        int total = 0;
+        int counter = 1;
+
+        for (int i = 0; i < w.size(); i++) {
+            if (total + w[i] > m) {
+                counter++;
+                total = w[i];
+            } else {
+                total += w[i];
+            }
+        }
+
+        return counter <= d;
+    }
+    
+    int shipWithinDays(vector<int>& weights, int days) {
+        int start = *max_element(weights.begin(), weights.end()); int end = accumulate(weights.begin(),weights.end(),0) + 1;
+        int ans = -1;
+        while(start<end)
+        {
+            int mid = (start + end)/2;
+            if(possible(weights,days,mid))
+            {
+                ans = mid;
+                end = mid;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
