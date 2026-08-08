971df45451f9786d4f4672fb32519fb8d70d7a2d
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:14:38 +0530

    Create 8-StringToIntegerAtoi

---
+++ 8-StringToIntegerAtoi/8-StringToIntegerAtoi.cpp
@@ -0,0 +1,31 @@
+// Last updated: 8/9/2026, 12:14:38 AM
+class Solution {
+public:
+    int myAtoi(string s) {
+        int ans = 0; int sign = 1; int i = 0;
+
+        while(s[i] == ' ')
+        {
+            i++;
+        }
+
+        if(i <= s.size() && s[i] == '-' || s[i] == '+')
+        {
+            sign = s[i]=='+' ? 1 : -1;
+            i++;
+        }
+
+        while(i < s.size() && isdigit(s[i]))
+        {
+            if(ans > INT_MAX/10 || (ans == INT_MAX/10 && s[i]>'7'))
+            {
+                return sign == -1 ? INT_MIN : INT_MAX;
+            }
+            ans = ans*10 + (s[i] - '0');
+            i++;
+        }
+
+        return ans*sign;
+
+    }
+};
