cc3729ac2d8640c5bea4b8ce934cf030b1740558
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:05:58 +0530

    Create 1549-LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit

---
+++ 1549-LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit/1549-LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.cpp
@@ -0,0 +1,25 @@
+// Last updated: 8/9/2026, 12:05:57 AM
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+
+        int i = 0, j = 0, ans = 1, diff = -1;
+        for (i = 0; i < nums.size() - 1; i++) {
+            if (nums.size() - i < ans) {
+                break;
+            }
+            int mini = nums[i];
+            int maxi = nums[i];
+            for (j = i + 1; j < nums.size(); j++) {
+                mini = min(nums[j], mini);
+                maxi = max(nums[j], maxi);
+                if (maxi - mini <= limit) {
+                    ans = max(ans, j - i + 1);
+                } else {
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
