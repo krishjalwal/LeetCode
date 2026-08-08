34a5b6ae0ea37f93cbfdee63c283c73bdfc1efce
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:03:25 +0530

    Create 2524-LargestPositiveIntegerThatExistsWithItsNegative

---
+++ 2524-LargestPositiveIntegerThatExistsWithItsNegative/2524-LargestPositiveIntegerThatExistsWithItsNegative.cpp
@@ -0,0 +1,24 @@
+// Last updated: 8/9/2026, 12:03:25 AM
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+
+        int ans = -1;
+
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            for(int j = i + 1 ; j < nums.size() ; j++)
+            {
+                if(nums[i] + nums[j] == 0)
+                {
+                    ans = nums[j];
+                    return ans;
+                }
+                
+            }
+        }
+        return ans;
+
+    }
+};
