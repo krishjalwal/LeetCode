cfb4d92a94ee023d352f5d00fa175b778d5824e1
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:02:40 +0530

    Create 3172-DivisibleAndNonDivisibleSumsDifference

---
+++ 3172-DivisibleAndNonDivisibleSumsDifference/3172-DivisibleAndNonDivisibleSumsDifference.cpp
@@ -0,0 +1,34 @@
+// Last updated: 8/9/2026, 12:02:40 AM
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        vector<int> divisible;
+        vector<int> nonDivisible;
+
+        for(int i = 1 ; i <= n ; i++)
+        {
+            if(i%m==0)
+            {
+                divisible.push_back(i);
+            }
+            else
+            {
+                nonDivisible.push_back(i);
+            }
+        }
+
+        int divisibleSum = 0 ;
+        int nonDivisibleSum = 0 ; 
+
+        for(auto it : divisible)
+        {
+            divisibleSum += it;
+        }
+        for(auto it : nonDivisible)
+        {
+            nonDivisibleSum += it;
+        }
+
+        return (nonDivisibleSum - divisibleSum);
+    }
+};
