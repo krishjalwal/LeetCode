c41707b68484a9c3607761f2a08b92891d22362b
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:04:49 +0530

    Create 1916-FindCenterOfStarGraph

---
+++ 1916-FindCenterOfStarGraph/1916-FindCenterOfStarGraph.cpp
@@ -0,0 +1,20 @@
+// Last updated: 8/9/2026, 12:04:49 AM
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        map<int,int>mpp;
+        for(int i =0 ;i<edges.size();i++){
+            mpp[edges[i][0]]++;
+            mpp[edges[i][1]]++;
+
+        }
+        int nodes = 0 ;
+        for(auto it:mpp){
+            nodes++;
+        }
+        for(auto it:mpp){
+          if(it.second==nodes-1)return it.first;
+        }
+        return 1;
+    }
+};
