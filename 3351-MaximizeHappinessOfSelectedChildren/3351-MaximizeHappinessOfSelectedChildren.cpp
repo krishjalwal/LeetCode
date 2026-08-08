f6c60ebe4b87ef76292961f338d768d19b04b52e
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:02:12 +0530

    Create 3351-MaximizeHappinessOfSelectedChildren

---
+++ 3351-MaximizeHappinessOfSelectedChildren/3351-MaximizeHappinessOfSelectedChildren.cpp
@@ -0,0 +1,19 @@
+// Last updated: 8/9/2026, 12:02:12 AM
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        // Sort the happiness values in non-increasing order
+        sort(happiness.rbegin(), happiness.rend());
+
+        long long total = 0;
+        for (int turn = 0; turn < k; ++turn) {
+            // Calculate the happiness contribution of the current child in this
+            // turn since his happinness has been decreased for all of the past
+            // rounds
+            long long current = max(happiness[turn] - turn, 0);
+            // Accumulate the happiness contribution
+            total += current;
+        }
+        return total;
+    }
+};
