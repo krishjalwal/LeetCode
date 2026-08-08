02fa017a21f32e3fc0102b77153956fdac23098e
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:08:58 +0530

    Create 605-CanPlaceFlowers

---
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.cpp
@@ -0,0 +1,21 @@
+// Last updated: 8/9/2026, 12:08:58 AM
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        while (n != 0) {
+            bool planted = false;
+            for (int i = 0; i < flowerbed.size(); i++) {
+                if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
+                    flowerbed[i] = 1;
+                    n--;
+                    planted = true;
+                    if (n == 0) // No need to continue if all flowers are planted
+                        break;
+                }
+            }
+            if (!planted) // No more suitable spots to plant
+                break;
+        }
+        return n == 0;
+    }
+};
