d19b46a5803175d5c1caa8e118d1b225b3611e96
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:06:48 +0530

    Create 1212-SequentialDigits

---
+++ 1212-SequentialDigits/1212-SequentialDigits.cpp
@@ -0,0 +1,20 @@
+// Last updated: 8/9/2026, 12:06:48 AM
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+    vector<int> result;
+
+    for (int i = 1; i < 10; ++i) {
+        int num = i;
+        for (int j = i + 1; j < 10; ++j) {
+            num = num * 10 + j;
+            if (num >= low && num <= high) {
+                result.push_back(num);
+            }
+        }
+    }
+    sort(result.begin(),result.end());
+
+    return result;
+    }
+};
