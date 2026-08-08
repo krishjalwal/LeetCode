63615488fce86719752cd7a81b6e9b8fc79f78bb
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:13:31 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -0,0 +1,20 @@
+// Last updated: 8/9/2026, 12:13:31 AM
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string, vector<string>> mp;
+
+        for (auto str : strs) {
+            string s = str;
+            sort(s.begin(), s.end());
+            mp[s].push_back(str);
+        }
+
+        vector<vector<string>> ans;
+        for (auto it : mp) {
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
