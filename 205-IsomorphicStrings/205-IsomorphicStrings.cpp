8e9a3699a0aefd7d0e63358c50f07af104c90b2f
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-09 00:11:32 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/205-IsomorphicStrings.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/9/2026, 12:11:32 AM
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length()!=t.length())return false;
+
+        int hash[256] = {0};
+        bool ismap[256] = {0};
+
+        for(int i = 0 ; i < s.length() ; i++)
+        {
+            if(hash[s[i]]==0 && ismap[t[i]]==0)
+            {
+                hash[s[i]] = t[i];
+                ismap[t[i]] = true;
+            }
+        }
+
+        for(int i = 0 ; i < s.length() ; i++)
+        {
+            if(hash[s[i]] != t[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
