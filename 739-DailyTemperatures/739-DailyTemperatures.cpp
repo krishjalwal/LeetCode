f080a7e50e29020f5edef0e5104b0c90ecbbeb72
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-15 23:25:24 +0530

    File updated at 8/15/2026, 11:25:23 PM

--- 739-DailyTemperatures/739-DailyTemperatures.cpp
+++ 739-DailyTemperatures/739-DailyTemperatures.cpp
@@ -1,32 +1,32 @@
-// Last updated: 8/9/2026, 12:08:29 AM
-class Solution {
-public:
-    vector<int> dailyTemperatures(vector<int>& temp) {\
-        int n = temp.size();
-        stack<int> st;
-        vector<int> res;
-        res.push_back(0);
-
-        st.push(n-1);
-        int curr;
-
-        for(int i = n-2; i >= 0 ; i--)
-        {        
-            while(!st.empty() && temp[st.top()] <= temp[i])
-            {
-                st.pop();
-            }
-            if(st.empty())
-            {
-                res.push_back(0);
-            }
-            else
-            {
-                res.push_back(abs(st.top() - i));
-            }           
-            st.push(i);
-        }
-        reverse(res.begin(),res.end());
-        return res;
-    }
-};
+// Last updated: 8/15/2026, 11:25:23 PM
+1class Solution {
+2public:
+3    vector<int> dailyTemperatures(vector<int>& temp) {\
+4        int n = temp.size();
+5        stack<int> st;
+6        vector<int> res;
+7        res.push_back(0);
+8
+9        st.push(n-1);
+10        int curr;
+11
+12        for(int i = n-2; i >= 0 ; i--)
+13        {        
+14            while(!st.empty() && temp[st.top()] <= temp[i])
+15            {
+16                st.pop();
+17            }
+18            if(st.empty())
+19            {
+20                res.push_back(0);
+21            }
+22            else
+23            {
+24                res.push_back(abs(st.top() - i));
+25            }           
+26            st.push(i);
+27        }
+28        reverse(res.begin(),res.end());
+29        return res;
+30    }
+31};
