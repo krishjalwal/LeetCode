76d9c5e5062bd2f92ab62f11245c9c450f1eb150
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-16 23:23:21 +0530

    File updated at 8/16/2026, 11:23:20 PM

--- 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.cpp
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.cpp
@@ -1,24 +1,28 @@
-// Last updated: 8/9/2026, 12:08:35 AM
-class Solution {
-public:
-    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        
-        int product = 1;
-        int low = 0 ;
-        int high = 0;
-        int count = 0;
-
-        while(high < nums.size())
-        {
-            product = product * nums[high];
-            while(product >= k)
-            {
-                product = product / nums[low];
-                low++;
-            }
-            count += (high-low+1);
-            high++;
-        }
-        return count;
-    }
-};
+// Last updated: 8/16/2026, 11:23:20 PM
+1class Solution {
+2public:
+3    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+4        
+5        int count = 0;
+6
+7        for(int i = 0 ; i < nums.size() ; i++)
+8        {
+9            int high = i;
+10            int product = 1;
+11            while(high<nums.size())
+12            {
+13                product = product * nums[high];
+14                if(product < k)
+15                {
+16                    count++;
+17                }
+18                else
+19                {
+20                    break;
+21                }
+22                high++;
+23            }
+24        }
+25        return count;
+26    }
+27};
