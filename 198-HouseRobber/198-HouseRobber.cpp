11f3e591c9500cf60b444d92ba93585961221881
Krish Jalwal <135513134+krishjalwal@users.noreply.github.com>
2026-08-29 07:25:23 +0530

    File updated at 8/29/2026, 7:25:23 AM

--- 198-HouseRobber/198-HouseRobber.cpp
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -1,17 +1,21 @@
-// Last updated: 8/9/2026, 12:11:42 AM
-class Solution {
-public:
-    int rob(vector<int>& nums) {
-        int sum1 = 0;
-        int sum2 = 0;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            if (i % 2 == 0) {
-                sum1 = max(sum1 + nums[i], sum2);
-            } else {
-                sum2 = max(sum2 + nums[i], sum1);
-            }
-        }
-        return max(sum1, sum2);
-    }
-};
+// Last updated: 8/29/2026, 7:25:23 AM
+1class Solution {
+2public:
+3    int rob(vector<int>& nums) {
+4        int n = nums.size();
+5        if(n==1)
+6        {
+7            return nums[0];
+8        }
+9        vector<int> store(n);
+10        store[0] = nums[0];
+11        store[1] = max(nums[0],nums[1]);
+12
+13        for(int i = 2 ; i < nums.size() ; i++)
+14        {
+15            store[i] = max(nums[i] + store[i-2],store[i-1]);
+16        }
+17
+18        return *max_element(store.begin(),store.end());
+19    }
+20};
